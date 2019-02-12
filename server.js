a9d4ae15177222d1f8ff57218e6b59755b9e759f
shurneyb <shurneyb@gmail.com>
2019-02-12 19:31:18 +0000

    changed category and changed limit to 10.

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ spotifyApi.clientCredentialsGrant()
 app.get('/search-track', function (request, response) {
   
   // Search for a track!
-  spotifyApi.searchTracks('track:forced hand', {limit: 1})
+  spotifyApi.searchTracks('track:composite', {limit: 1})
     .then(function(data) {
     
       // Send the first (only) track object
@@ -64,7 +64,7 @@ app.get('/category-playlists', function (request, response) {
   
   // Get playlists from a browse category
   // Find out which categories are available here: https://beta.developer.spotify.com/console/get-browse-categories/
-  spotifyApi.getPlaylistsForCategory('jazz', { limit : 5 })
+  spotifyApi.getPlaylistsForCategory('rock', { limit : 10 })
     .then(function(data) {
     
     // Send the list of playlists
